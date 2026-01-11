556937f0f1baea38eda294141efd1a17e22be07e
kanha2307 <144936330+kanha2307@users.noreply.github.com>
2026-01-11 23:06:30 +0530

    improved the table

--- JSON_TASK/dt.js
+++ JSON_TASK/dt.js
@@ -1,151 +1,142 @@
-var nameInput = document.getElementById('name');
-var orderInput = document.getElementById('order');
-var productInput = document.getElementById('product');
-var StatusBtn = document.getElementById('status')
-var maxInput = document.getElementById('max')
-console.log('max', maxInput);
+var nameInput = document.getElementById("name");
+var orderInput = document.getElementById("order");
+var productInput = document.getElementById("product");
+var StatusBtn = document.getElementById("status");
+var maxInput = document.getElementById("max");
+console.log("max", maxInput);
 
-var minInput = document.getElementById('min')
-var SearchBtn = document.getElementById('search')
+var minInput = document.getElementById("min");
+var SearchBtn = document.getElementById("search");
+var clearBtn = document.getElementById("clear");
 
-var nameFilter = ''
-var orderFilter = ''
-var statusFilter = ''
-var productFilter = ''
-var minFilter
-var maxFilter
+var nameFilter = "";
+var orderFilter = "";
+var statusFilter = "";
+var productFilter = "";
+var minFilter;
+var maxFilter;
 
+const table = new DataTable("#example", {
+  ajax: {
+    url: "./data.json",
+    dataSrc: function (json) {
+      let filteredData = json.data;
 
-const table = new DataTable('#example', {
-    ajax: {
-        url: './data.json',
-        dataSrc:
+      if (nameFilter) {
+        filteredData = filteredData.filter((item) =>
+          item.customer.firstname
+            .toLowerCase()
+            .includes(nameFilter.toLowerCase())
+        );
+      }
+      if (productFilter) {
+        filteredData = filteredData.filter((item) =>
+          item.carts.map((items) => items.product_name.includes(productFilter))
+        );
+      }
 
-            function (json) {
+      if (orderFilter) {
+        filteredData = filteredData.filter((item) =>
+          item.order_number
+            .toString()
+            .toLowerCase()
+            .includes(orderFilter.toLowerCase())
+        );
+      }
 
-                let filteredData = json.data;
+      if (maxFilter || minFilter) {
+        filteredData = filteredData.filter(
+          (item) =>
+            minFilter <= parseFloat(item.total) &&
+            parseFloat(item.total) <= maxFilter
+        );
+        console.log("filter", filteredData);
+      }
+      if (statusFilter === "All") {
+        return filteredData;
+      }
 
-
-                if (nameFilter) {
-                    filteredData = filteredData.filter(item =>
-                        item.customer.firstname
-                            .toLowerCase()
-                            .includes(nameFilter.toLowerCase())
-                    );
-                }
-                if (productFilter) {
-                    filteredData = filteredData.filter(item =>
-                        item.carts.map(items => items.product_name.includes(productFilter))
-
-
-                    );
-
-
-                }
-
-                if (orderFilter) {
-                    filteredData = filteredData.filter(item =>
-                        item.order_number
-                            .toString().toLowerCase()
-                            .includes(orderFilter.toLowerCase())
-                    );
-                }
-
-                if (maxFilter || minFilter) {
-
-
-                    filteredData = filteredData.filter(item => minFilter <= parseFloat(item.total) && parseFloat(item.total) <= maxFilter);
-                    console.log("filter", filteredData);
-
-                }
-                if (statusFilter === "All") {
-                    return filteredData
-                }
-
-                if (statusFilter) {
-
-
-                    filteredData = filteredData.filter(item =>
-                        item.status
-                            .toLowerCase()
-                            .includes(statusFilter.toLowerCase())
-                    );
-                }
-                return filteredData;
-            }
+      if (statusFilter) {
+        filteredData = filteredData.filter((item) =>
+          item.status.toLowerCase().includes(statusFilter.toLowerCase())
+        );
+      }
+      return filteredData;
     },
-    pageLength: 50,
-    lengthChange: false,
-    columns: [
-        {
-            className: 'dt-control',
-            orderable: false,
-            data: null,
-            defaultContent: ''
-        },
-        {
+  },
+  pageLength: 10,
+  lengthChange: false,
+  columns: [
+    {
+      className: "dt-control",
+      orderable: false,
+      data: null,
+      defaultContent: "",
+    },
+    {
+      render: function (data, type, row, meta) {
+        return meta.settings._iDisplayStart + meta.row + 1;
+      },
+    },
 
+    { data: "customer.firstname" },
 
-            render: function (data, type, row, meta) {
-
-
-                return meta.settings._iDisplayStart + meta.row + 1;
-            }
-        },
-
-        { data: 'customer.firstname' },
-
-        { data: 'customer.lastname' },
-        { data: 'order_number' },
-        { data: 'total' },
-        { data: 'status' },
-        { data: 'payment_status' },
-        { data: 'source' },
-        { data: 'customer.customer_code' },
-        { data: 'customer.mobile' }
-    ],
-    order: [[1, 'asc']]
+    { data: "customer.lastname" },
+    { data: "order_number" },
+    { data: "total" },
+    { data: "status" },
+    { data: "payment_status" },
+    { data: "source" },
+    { data: "customer.customer_code" },
+    { data: "customer.mobile" },
+  ],
+  order: [[1, "asc"]],
 });
 
-
-nameInput.addEventListener('keyup', function () {
-    nameFilter = this.value;
-    table.ajax.reload();
+nameInput.addEventListener("keyup", function () {
+  nameFilter = this.value;
+  table.ajax.reload();
 });
 
-orderInput.addEventListener('keyup', function () {
-    orderFilter = this.value;
-    table.ajax.reload();
+orderInput.addEventListener("keyup", function () {
+  orderFilter = this.value;
+  table.ajax.reload();
 });
 
-productInput.addEventListener('keyup', function () {
-    productFilter = this.value;
-    table.ajax.reload();
+productInput.addEventListener("keyup", function () {
+  productFilter = this.value;
+  table.ajax.reload();
 });
 
+SearchBtn.addEventListener("click", () => {
+  minFilter = minInput.value;
+  console.log("min", minFilter);
 
-SearchBtn.addEventListener('click', () => {
-    minFilter = minInput.value
-    console.log("min", minFilter);
+  maxFilter = maxInput.value;
+  console.log("max", maxFilter);
+  table.ajax.reload();
+});
 
-    maxFilter = maxInput.value
-    console.log("max", maxFilter);
-    table.ajax.reload();
-
-})
-StatusBtn.addEventListener('click', () => {
-    console.log("status", StatusBtn.value);
-    statusFilter = StatusBtn.value
-    table.ajax.reload();
-})
-
+clearBtn.addEventListener("click", () => {
+  console.log("clear");
+  nameInput.value = "";
+  orderInput.value = "";
+  productInput.value = "";
+  minInput.value = "";
+  maxInput.value = "";
+});
+StatusBtn.addEventListener("click", () => {
+  console.log("status", StatusBtn.value);
+  statusFilter = StatusBtn.value;
+  table.ajax.reload();
+});
 
 function format(d) {
-    console.log("d", d);
+  console.log("d", d);
 
-
-    let loop = d.carts.map((item, index) => {
-        return `
+  let loop = d.carts
+    .map((item, index) => {
+      return `
      
 <div class="w-1/4 flex rounded overflow-hidden shadow-lg bg-white m-4"> 
       <img class="w-1/2 h-48 object-cover" src="${item.product_image}" alt="${item.product_name}"> 
@@ -157,21 +148,18 @@ function format(d) {
     </div>
        
     
-    `
-    }).join('');
-    return `<div class="w-full flex ">${loop}</div>`;
-
+    `;
+    })
+    .join("");
+  return `<div class="w-full flex ">${loop}</div>`;
 }
-table.on('click', 'tbody td.dt-control', function (e) {
-    let tr = e.target.closest('tr');
-    let row = table.row(tr);
+table.on("click", "tbody td.dt-control", function (e) {
+  let tr = e.target.closest("tr");
+  let row = table.row(tr);
 
-    if (row.child.isShown()) {
-
-        row.child.hide();
-    }
-    else {
-
-        row.child(format(row.data())).show();
-    }
+  if (row.child.isShown()) {
+    row.child.hide();
+  } else {
+    row.child(format(row.data())).show();
+  }
 });
